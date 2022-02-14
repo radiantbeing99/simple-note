1d4f3a833acb61d39570b38baa017536b15898ce
Win-9 <80390524+Win-9@users.noreply.github.com>
2022-02-14 15:45:38 +0900

    설정추가

---
+++ src/main/java/simple/simplenote/domain/JpaMain.java
@@ -0,0 +1,35 @@
+package simple.simplenote.domain;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+@Transactional
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setAge(10);
+            member.setId("memberA");
+
+            em.persist(member);
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/simple/simplenote/domain/Member.java
@@ -0,0 +1,22 @@
+package simple.simplenote.domain;
+
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private String id;
+    private int age;
+
+
+}
