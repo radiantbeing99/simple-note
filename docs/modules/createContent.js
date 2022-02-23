e6b0ed68bcca43e362fb060c365160ef9185e62d
INHWA KIM <radiantbeing99@gmail.com>
2022-02-23 23:15:39 +0900

    Create, Update 디자인 리팩토링

--- docs/modules/createContent.js
+++ docs/modules/createContent.js
@@ -1,6 +1,8 @@
+// FIXME: Create Submit 이후 active 안되는 문제
+
 import { paintContent, setCurrentContent } from "./contentFunctions.js";
 import { getAPI, postAPI } from "./controlBackEndAPI.js";
-import { paintTOC } from "./paintTOC.js";
+import { activeButton, paintTOC } from "./paintTOC.js";
 
 const _createButton = document.querySelector(".create-button");
 const _contentForm = document.querySelector(".content-form");
@@ -14,66 +16,66 @@ function handleButtonOpen(MAX_CONTENTS_NUM) {
 
   const NEW_ID = MAX_CONTENTS_NUM + 1;
 
-  const form = document.createElement("form");
+  const _form = document.createElement("form");
   const _titleDiv = document.createElement("div");
   const _titleLabel = document.createElement("label");
   const _titleInput = document.createElement("input");
   const _descriptionDiv = document.createElement("div");
   const _descriptionLabel = document.createElement("label");
-  const header = document.createElement("h3");
-  const hidden = document.createElement("input");
+  const _header = document.createElement("h3");
+  const _hidden = document.createElement("input");
   const _descriptionTextarea = document.createElement("textarea");
-  const submit = document.createElement("input");
+  const _submit = document.createElement("input");
 
   // form 설정
-  form.classList.add("create-form");
-  form.addEventListener("submit", handleSubmit);
+  _form.classList.add("create-form");
+  _form.addEventListener("submit", handleSubmit);
   // header 설정
-  header.innerText = "Create-Form";
+  _header.innerText = "Create-Form";
   // hidden 설정
-  hidden.classList.add("create-id");
-  hidden.setAttribute("type", "hidden");
-  hidden.setAttribute("name", "id");
-  hidden.setAttribute("value", String(NEW_ID));
+  _hidden.classList.add("create-id");
+  _hidden.setAttribute("type", "hidden");
+  _hidden.setAttribute("name", "id");
+  _hidden.setAttribute("value", String(NEW_ID));
   // titleDiv 설정
   _titleDiv.classList.add("mb-3");
   // titleLabel 설정
-  _titleLabel.setAttribute("for", "inputContentTitle");
+  _titleLabel.setAttribute("for", "create-inputContentTitle");
   _titleLabel.classList.add("form-label");
   _titleLabel.innerText = "Title";
   // titleInput 설정
   _titleInput.classList.add("create-title", "form-control");
-  _titleInput.id = "inputContentTitle";
+  _titleInput.id = "create-inputContentTitle";
   _titleInput.setAttribute("type", "text");
   _titleInput.setAttribute("required", "");
   _titleInput.setAttribute("name", "title");
   // descriptionDiv 설정
   _descriptionDiv.classList.add("mb-3");
   // descriptionLabel 설정
-  _descriptionLabel.setAttribute("for", "inputDescription");
+  _descriptionLabel.setAttribute("for", "create-inputDescription");
   _descriptionLabel.classList.add("form-label");
   _descriptionLabel.innerText = "Description";
   // descriptionTextarea 설정
   _descriptionTextarea.classList.add("create-description", "form-control");
-  _descriptionTextarea.id = "inputDescription";
+  _descriptionTextarea.id = "create-inputDescription";
   _descriptionTextarea.setAttribute("required", "");
   _descriptionTextarea.setAttribute("name", "description");
   _descriptionTextarea.setAttribute("rows", "10");
   // submit 설정
-  submit.classList.add("create-submit", "btn", "btn-primary");
-  submit.setAttribute("type", "submit");
-  submit.setAttribute("value", "submit");
+  _submit.classList.add("create-submit", "btn", "btn-primary");
+  _submit.setAttribute("type", "submit");
+  _submit.setAttribute("value", "submit");
 
   _titleDiv.appendChild(_titleLabel);
   _titleDiv.appendChild(_titleInput);
-  form.appendChild(header);
-  form.appendChild(_titleDiv);
-  form.appendChild(hidden);
+  _form.appendChild(_header);
+  _form.appendChild(_titleDiv);
+  _form.appendChild(_hidden);
   _descriptionDiv.appendChild(_descriptionLabel);
   _descriptionDiv.appendChild(_descriptionTextarea);
-  form.appendChild(_descriptionDiv);
-  form.appendChild(submit);
-  _contentForm.appendChild(form);
+  _form.appendChild(_descriptionDiv);
+  _form.appendChild(_submit);
+  _contentForm.appendChild(_form);
 }
 
 function handleButtonClose() {
@@ -88,11 +90,11 @@ function handleSubmit(event) {
   const title = document.querySelector(".create-title");
   const description = document.querySelector(".create-description");
   const createdContent = { id: id.value, title: title.value, description: description.value };
-  console.log(createdContent);
   postAPI("contents", createdContent, paintTOC);
   setCurrentContent(createdContent);
   paintContent();
   handleButtonClose();
+  // active 코드
 }
 
 export function createContent() {
--- docs/modules/paintTOC.js
+++ docs/modules/paintTOC.js
@@ -7,25 +7,23 @@ const _updateButton = document.querySelector(".update-button");
 const _deleteButton = document.querySelector(".delete-button");
 
 const state = {
-  isActive: false,
   isDisabledDeleted: false,
 };
 
 function handleClickAnchor(event, contentID) {
-  const _listGroupItems = _tableOfContents.querySelectorAll("a");
-  const _anchor = event.target;
-
   event.preventDefault();
+
   if (state.isDisabledDeleted === false) {
     _updateButton.removeAttribute("disabled");
     _deleteButton.removeAttribute("disabled");
     state.isDisabledDeleted = true;
   }
 
+  const _anchor = event.target;
+  const _listGroupItems = _tableOfContents.querySelectorAll("a");
   _listGroupItems.forEach((item) => {
     item.classList.remove("active");
   });
-
   _anchor.classList.add("active");
 
   getAPI(`contents/${contentID}`, (content) => {
@@ -35,13 +33,17 @@ function handleClickAnchor(event, contentID) {
   });
 }
 
+export function activeButton(element) {
+  element.classList.add("active");
+}
+
 export function paintTOC() {
   const paint = getAPI("contents/toc", (data) => {
     const contents = data.contentsTOC;
     contents.forEach((content) => {
       const a = document.createElement("a");
       a.innerText = content.title;
-      a.setAttribute("href", content.id);
+      a.setAttribute("id", `content-${content.id}`);
       a.classList.add("list-group-item", "list-group-item-action");
       a.addEventListener("click", (event) => handleClickAnchor(event, content.id));
       _tableOfContents.appendChild(a);
