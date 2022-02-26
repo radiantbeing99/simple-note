0aec9ad5dec880d241b3ac1e9f40e173954863d5
INHWA KIM <radiantbeing99@gmail.com>
2022-02-27 00:25:16 +0900

    명언 출력 기능 기반

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ import { createContent } from "./modules/createContent.js";
 import { updateContent } from "./modules/updateContent.js";
 import { deleteContent } from "./modules/deleteContent.js";
 import { paintLoginForm } from "./modules/paintLoginForm.js";
+import { paintQuote } from "./modules/paintQuote.js";
 
 const _updateButton = document.querySelector(".update-button");
 const _deleteButton = document.querySelector(".delete-button");
 _updateButton.setAttribute("disabled", "");
 _deleteButton.setAttribute("disabled", "");
 
+console.log("hello");
 paintLoginForm();
+paintQuote();
 createContent();
 updateContent();
 deleteContent();
---
+++ modules/paintQuote.js
@@ -0,0 +1,3 @@
+export function paintQuote() {
+  console.log("quote");
+}
